45475a20f8a37888de87b55fe71fd0c7a22cba47
KristofferC <kcarlsson89@gmail.com>
2020-08-13 12:48:24 +0200

    add updates from Pkg.TOML to print

--- src/print.jl
+++ src/print.jl
@@ -1,3 +1,5 @@
+# This file is a part of Julia. License is MIT: https://julialang.org/license
+
 "Identify if character in subset of bare key symbols"
 isbare(c::AbstractChar) = 'A' <= c <= 'Z' || 'a' <= c <= 'z' || isdigit(c) || c == '-' || c == '_'
 
@@ -16,52 +18,55 @@ function printkey(io::IO, keys::Vector{String})
     end
 end
 
-function printvalue(io::IO, value; sorted=false)
-    if isa(value, AbstractDict)
-        _print(io, value, sorted=sorted)
-    elseif isa(value, Array)
-        Base.print(io, "[")
-        for (i, x) in enumerate(value)
-            i != 1 && Base.print(io, ", ")
-            if isa(x, AbstractDict)
-                _print(io, x, sorted=sorted)
-            else
-                printvalue(io, x, sorted=sorted)
-            end
+function printvalue(io::IO, value::AbstractArray; sorted=false)
+    Base.print(io, "[")
+    for (i, x) in enumerate(value)
+        i != 1 && Base.print(io, ", ")
+        if isa(x, AbstractDict)
+            _print(io, x, sorted=sorted)
+        else
+            printvalue(io, x, sorted=sorted)
         end
-        Base.print(io, "]")
-    elseif isa(value, AbstractString)
-        Base.print(io, "\"$(escape_string(value))\"")
-    elseif isa(value, Dates.DateTime)
-        Base.print(io, Dates.format(value, "YYYY-mm-ddTHH:MM:SS.sssZ"))
-    else
-        Base.print(io, value)
     end
+    Base.print(io, "]")
 end
+printvalue(io::IO, value::AbstractDict; sorted=false) =
+    _print(io, value, sorted=sorted)
+printvalue(io::IO, value::DateTime; sorted=false) =
+    Base.print(io, Dates.format(value, dateformat"YYYY-mm-dd\THH:MM:SS.sss\Z"))
+printvalue(io::IO, value::Bool; sorted=false) =
+    Base.print(io, value ? "true" : "false")
+printvalue(io::IO, value::Integer; sorted=false) =
+    Base.print(io, Int(value))  # TOML specifies 64-bit signed long range for integer
+printvalue(io::IO, value::AbstractFloat; sorted=false) =
+    Base.print(io, Float64(value))  # TOML specifies IEEE 754 binary64 for float
+printvalue(io::IO, value; sorted=false) =
+    Base.print(io, "\"$(escape_string(string(value)))\"")
 
 is_table(value)           = isa(value, AbstractDict)
-is_array_of_tables(value) = isa(value, Array) && length(value) > 0 && isa(value[1], AbstractDict)
+is_array_of_tables(value) = isa(value, AbstractArray) &&
+                            length(value) > 0 && isa(value[1], AbstractDict)
 is_tabular(value)         = is_table(value) || is_array_of_tables(value)
 
 function _print(io::IO, a::AbstractDict,
     ks::Vector{String} = String[];
-    indent::Int = length(ks),
+    indent::Int = 0,
     first_block::Bool = true,
     sorted::Bool = false,
-    by::Function = identity,
+    by = identity,
 )
     akeys = keys(a)
     if sorted
-        akeys = sort!(collect(akeys), by=by)
+        akeys = sort!(collect(akeys), by = by)
     end
 
     for key in akeys
         value = a[key]
-        is_tabular(value) && continue # skip tables
-        Base.print(io, repeat("    ", max(0, indent-1)))
-        printkey(io, [key])
+        is_tabular(value) && continue
+        Base.print(io, ' '^4max(0,indent-1))
+        printkey(io, [String(key)])
         Base.print(io, " = ") # print separator
-        printvalue(io, value, sorted=sorted)
+        printvalue(io, value, sorted = sorted)
         Base.print(io, "\n")  # new line?
         first_block = false
     end
@@ -69,9 +74,8 @@ function _print(io::IO, a::AbstractDict,
     for key in akeys
         value = a[key]
         if is_table(value)
-            push!(ks, key)
-            # print table header if values are not tabular or do not exist
-            header = !all(is_tabular(v) for v in values(value)) || length(value) == 0
+            push!(ks, String(key))
+            header = !all(is_tabular(v) for v in values(value))
             if header
                 # print table
                 first_block || println(io)
@@ -81,20 +85,21 @@ function _print(io::IO, a::AbstractDict,
                 printkey(io, ks)
                 Base.print(io,"]\n")
             end
-            _print(io, value, ks, indent=indent+header, first_block=header, by=by, sorted=sorted)
+            # Use runtime dispatch here since the type of value seems not to be enforced other than as AbstractDict
+            Base.invokelatest(_print, io, value, ks; indent = indent + header, first_block = header, sorted = sorted, by = by)
             pop!(ks)
         elseif is_array_of_tables(value)
             # print array of tables
             first_block || println(io)
             first_block = false
-            push!(ks, key)
+            push!(ks, String(key))
             for v in value
                 Base.print(io, ' '^4indent)
                 Base.print(io,"[[")
                 printkey(io, ks)
                 Base.print(io,"]]\n")
-                !isa(v, AbstractDict) && Base.error("array should contain only tables")
-                _print(io, v, ks, indent=indent+1, sorted=sorted, by=by)
+                !isa(v, AbstractDict) && error("array should contain only tables")
+                Base.invokelatest(_print, io, v, ks; indent = indent + 1, sorted = sorted, by = by)
             end
             pop!(ks)
         end
